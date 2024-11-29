e29fda91ca2c8ebcf39761a02959286567758252
admin <rmsgh501@gmail.com>
2024-11-29 21:47:06 +0900

    Initial commit

---
+++ src/AppStart.java
@@ -0,0 +1,9 @@
+import com.koreait.coffee.view.MainView;
+
+public class AppStart {
+    public static void main(String[] args) {
+        MainView mainView = new MainView();
+
+        mainView.mainView();
+    }
+}
